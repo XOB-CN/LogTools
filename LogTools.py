7fc71ea257af86e440e83987145615f66c98c8c3
XOB-CN <748689323@qq.com>
2020-03-05 22:36:21 +0800

    Update LogTools.py
    coding LogTools function

--- LogTools.py
+++ LogTools.py
@@ -7,7 +7,7 @@ class LogTools(QtWidgets.QWidget, Ui_Form):
     """
     LogTools GUI class
     """
-    # 类变量
+    # 支持的软件产品
     cbx_data = {
         'HPE':{'BigData':['IDOL',]},
         'MicroFocus':{'ITOM':['OA','OBM/OMi']},
@@ -16,7 +16,7 @@ class LogTools(QtWidgets.QWidget, Ui_Form):
     def __init__(self, parent=None, *args, **kwargs):
         super().__init__(parent, *args, **kwargs)
 
-    def set_cbx_default(self):
+    def load_cbx_company(self):
         cbx_comp_list = []
         for i in self.cbx_data:
             cbx_comp_list.append(i)
@@ -30,9 +30,13 @@ class LogTools(QtWidgets.QWidget, Ui_Form):
         self.cbx_category.addItems(cbx_cate_list)
 
     def load_cbx_product(self):
-        cbx_prod_list = self.cbx_data.get(self.cbx_category.currentText())
-        print(cbx_prod_list)
+        cbx_prod_list = self.cbx_data.get(self.cbx_company.currentText()).get(self.cbx_category.currentText())
         self.cbx_product.addItems(cbx_prod_list)
+
+    # 槽函数
+    def ref_cate_prod(self):
+        self.load_cbx_category()
+        self.load_cbx_product()
 
 if __name__ == "__main__":
     import sys
@@ -41,11 +45,7 @@ if __name__ == "__main__":
     ui = LogTools()
     ui.setupUi(From)
     # 设置 QComboBox（cbx_company）组件的默认内容
-    ui.set_cbx_default()
-
-    # 临时代码
-    ui.load_cbx_category()
-    ui.load_cbx_product()
+    ui.load_cbx_company()
 
     # 设置窗口标题
     From.setWindowTitle('LogTools')
