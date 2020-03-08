6ee907da10c9e87cfd0773af5b65e33933a57f13
XOB-CN <748689323@qq.com>
2020-03-08 23:20:36 +0800

    Update LogTools.py
    coding LogTools function

--- LogTools.py
+++ LogTools.py
@@ -7,8 +7,12 @@ if __name__ == '__main__':
     import sys
     app = QApplication(sys.argv)
     gui = LogApp()
+
+    # 初始化数据列表
     gui.load_cbx_company()
     gui.load_cbx_category()
     gui.load_cbx_product()
+
+    gui.setWindowTitle('LogTools')
     gui.show()
     sys.exit(app.exec_())
