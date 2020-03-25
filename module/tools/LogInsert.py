d491bbf56ac1beadfaf8af2d7930004f602baf91
XOB-CN <748689323@qq.com>
2020-03-25 17:53:58 +0800

    Update LogInsert.py

--- module/tools/LogInsert.py
+++ module/tools/LogInsert.py
@@ -10,17 +10,25 @@ class LogRead(QThread):
         self.task_info = task_info
 
     def run(self):
+        '''
+        返回待分析的文件列表
+        :return: list
+        '''
         filepath = self.task_info.get('file_path')
         if filepath != '':
+            # 如果 file_type 的值是 file, 直接返回文件列表
             self.task_info['file_type'] = 'file'
-            # 待分析的文件 URL 列表
             self.task_info['logs_list'] = [self.task_info.get('file_path'),]
         else:
+            # 如果 file_type 的值是 dir, 则需要做进一步的判断
             self.task_info['file_type'] = 'dir'
-            # 待分析的文件 URL 列表
+            url_file_list = []
+            # os.walk 方法将返回三个值, 分别为 '当前目录', '当前目录的子目录', '当前目录所包含的文件'
             for root, dirs, files in os.walk(self.task_info.get('dir_path')):
-                print(root)
-                print(dirs)
-                print(files)
+                for file in files:
+                    url_file = os.path.join(root, file)
+                    url_file = os.path.abspath(url_file)
+                    url_file_list.append(url_file)
+            print(url_file_list)
 
         print(self.task_info)
