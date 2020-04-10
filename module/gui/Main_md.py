cbb941d7e61056b0b3ced82321a2c41b1290bbc0
XOB-CN <748689323@qq.com>
2020-04-10 17:04:58 +0800

    coding QtableView function

--- module/gui/Main_md.py
+++ module/gui/Main_md.py
@@ -10,6 +10,7 @@ class LogMain(QMainWindow, Ui_MainWindow):
     LogTools Main class
     """
     num_new_query = 1
+    num_new_result = 1
     singal_btn_import = pyqtSignal(str, str, str)
 
     # 接收产品分类
@@ -62,10 +63,9 @@ class LogMain(QMainWindow, Ui_MainWindow):
     def slot_new_query(self):
         # 新建的 Tab 名字
         self.num_new_query += 1
-        tablabel = 'Tab' + str(self.num_new_query)
+        tablabel = 'SQL_Query_' + str(self.num_new_query)
         tlaylout = 'Layout' + str(self.num_new_query)
         textedit = 'sqlEdit' + str(self.num_new_query)
-
         # 生成 QWidget 以及 QTextEdit 中的内容
         self.tab_page = QWidget()
         self.tab_page.setObjectName(tablabel)
@@ -74,7 +74,6 @@ class LogMain(QMainWindow, Ui_MainWindow):
         self.tab_text = QTextEdit(self.tab_page)
         self.tab_text.setObjectName(textedit)
         self.tab_layout.addWidget(self.tab_text, 0, 0, 1, 1)
-
         # 将生成的 QWidget 中的内容追加到 QTabWidget 中
         self.tabQuery.addTab(self.tab_page, tablabel)
         self.tabQuery.setObjectName(tablabel)
@@ -82,15 +81,37 @@ class LogMain(QMainWindow, Ui_MainWindow):
 
     # 执行查询语句, 并且返回结果
     def slot_run_sql_query(self):
+        # 获取 sqlEdit 上一级的对象名
+        sqltitle = self.tabQuery.currentWidget().objectName()
         # sqlEdit 返回的是当前激活的 QTextEdit 对象
         sqlEdit = self.tabQuery.currentWidget().findChild(QTextEdit)
         sql_str = sqlEdit.toPlainText()
+
         # 判断获取的内容
         if len(sql_str) == 0:
             print('no input anything')
         else:
             print('****** execute sql query ******')
             print(sql_str)
+
+        # 生成 QtableView 对象:self.tab_view ########################
+        self.num_new_result += 1
+        tablabel = sqltitle.replace('Query','Result')
+        tlaylout = 'LayResult' + str(self.num_new_result)
+        sql_rest = 'sqlResult' + str(self.num_new_result)
+        # 生成 QWidget 以及 QtableView 中的内容
+        self.tab_page_result = QWidget()
+        self.tab_page_result.setObjectName(tablabel)
+        self.tab_layout_result = QGridLayout(self.tab_page_result)
+        self.tab_layout_result.setObjectName(tlaylout)
+        self.tab_view = QTableView(self.tab_page_result)
+        self.tab_view.setObjectName(sql_rest)
+        self.tab_layout_result.addWidget(self.tab_view, 0, 0, 1, 1)
+        # 将生成的 QWidget 中的内容追加到 QTabWidget 中
+        self.tabResult.addTab(self.tab_page_result, tablabel)
+        self.tabResult.setObjectName(tablabel)
+        self.tabResult.setCurrentIndex(self.tabResult.count() - 1)
+        ##############################################################
 
     # 选中 dblist 的表时, 执行 sql 查询
     def slot_dblist_sql_query(self):
@@ -103,6 +124,10 @@ class LogMain(QMainWindow, Ui_MainWindow):
     # SQL Query Tab 关闭函数
     def slot_tab_sql_close(self, index):
         self.tabQuery.removeTab(index)
+
+    # SQL Result Tab 关闭函数
+    def slot_tab_result_close(self, index):
+        self.tabResult.removeTab(index)
 
     # 读取日志文件
     def slot_action_import(self):
