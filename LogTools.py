fc1f146f8c71acbf29d6790935217bb157cfb43d
XOB-CN <748689323@qq.com>
2020-04-22 16:09:43 +0800

    Update LogTools.py

--- LogTools.py
+++ LogTools.py
@@ -52,7 +52,7 @@ if __name__ == '__main__':
         # 将初始界面获取的产品分类数据传递到 LogTools 主界面里
         guiMain.product_type = [company_name, category_name, product_name]
         # 设置软件标题
-        guiMain.setWindowTitle(category_name + ' ' + product_name + ' LogTools ' + 'v0.8.0 Beta')
+        guiMain.setWindowTitle(category_name + ' ' + product_name + ' LogTools ' + ' Beta')
         # 显示 LogTools 主界面
         guiMain.show()
         # 隐藏 LogTools 初始界面
