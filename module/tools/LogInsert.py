c259f1e7f90d603f3f1e131529c8187b007556d6
XOB-CN <748689323@qq.com>
2020-04-23 23:20:33 +0800

    Update LogInsert.py

--- module/tools/LogInsert.py
+++ module/tools/LogInsert.py
@@ -3,4 +3,22 @@
 from PyQt5.Qt import *
 
 class LogInsert(QThread):
-    pass
+    # 发射自定义信号, 说明已开启一个任务
+    singal_log_process = pyqtSignal(int)
+
+    def __init__(self, parent=None, task_data=None):
+        super().__init__(parent)
+        self.task_data = task_data
+
+    def run(self):
+
+        # 匹配产品规则
+        if self.task_data.get('product_type') == 'MicroFocus-ITOM-OA':
+            from module.rules import MicroFocus_ITOM_OA_FileRule as FileRule
+            self.file_rule = FileRule.FileRule
+            self.fileblk_rule = FileRule.FileBlkRule
+
+        elif self.task_data.get('product_type') == 'MicroFocus-ITOM-OBM/OMi':
+            from module.rules import MicroFocus_ITOM_OBM_FileRule as FileRule
+            self.file_rule = FileRule.FileRule
+            self.fileblk_rule = FileRule.FileBlkRule
