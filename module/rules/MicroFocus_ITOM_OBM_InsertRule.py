7f8713a30125419cee182be250d5cba13cee0b51
XOB-CN <748689323@qq.com>
2020-07-19 03:44:18 +0800

    fix bugs

--- module/rules/MicroFocus_ITOM_OBM_InsertRule.py
+++ module/rules/MicroFocus_ITOM_OBM_InsertRule.py
@@ -65,6 +65,8 @@ class ITOM_OBM():
                             # 判断该行日志是否符合格式
                             if re.findall('INFO.*HEAP.*\[USAGE.*FREE.*TOTAL.*MAX.*\].*PERM.*\[USAGE.*FREE.*MAX.*\]|INFO.*HEAP.*\[USAGE.*FREE.*TOTAL.*MAX.*\].*NON-HEAP.*\[USAGE.*FREE.*MAX.*\]', line):
                                 log_level = line.split(' - ')[0].split(' ')[2].strip()
+                                if log_level == '':
+                                    log_level = line.split(' - ')[0].split('[')[0].strip().split(' ')[-1]
                                 log_time = (line.split(' - ')[0].split(' ')[0].strip() + ' ' + line.split(' - ')[0].split(' ')[1].strip()).replace(',', '.')
                                 log_time = sql_write.sqlite_to_datetime(log_time)
                                 log_line = str(log_num)
@@ -88,6 +90,32 @@ class ITOM_OBM():
                                                 'non_heap_committed':'Null',
                                                 'non_heap_max':perm_max,
                                                 'non_heap_free':perm_free,
+                                                'othermsg':othermsg,})
+
+                            elif re.findall('INFO.*HEAP.*\[USAGE.*FREE.*MAX.*\].*NON-HEAP.*\[USAGE.*FREE.*MAX.*\]', line):
+                                log_level = line.split(' - ')[0].split(' ')[2].strip()
+                                log_time = (line.split(' - ')[0].split(' ')[0].strip() + ' ' + line.split(' - ')[0].split(' ')[1].strip()).replace(',', '.')
+                                log_time = sql_write.sqlite_to_datetime(log_time)
+                                log_line = str(log_num)
+                                heap_usage = line.split('; ')[0].split('USAGE:')[-1].split(',')[0].strip()
+                                heap_free = line.split('; ')[0].split('FREE:')[-1].split(',')[0].strip()
+                                heap_max = line.split('; ')[0].split('MAX:')[-1].split(',')[0].strip()[:-1]
+                                non_heap_usage = line.split('; ')[1].split('USAGE:')[-1].split(',')[0].strip()
+                                non_heap_free = line.split('; ')[1].split('FREE:')[-1].split(',')[0].strip()
+                                non_heap_max = line.split('; ')[1].split('MAX:')[-1].split(',')[0].strip()[:-1]
+                                othermsg = line.split('; ', 2)[-1]
+                                logdata.append({'logfile':self.filepath,
+                                                'loglevel':log_level,
+                                                'logtime':log_time,
+                                                'logline':log_line,
+                                                'heap_used':heap_usage,
+                                                'heap_committed':'Null',
+                                                'heap_max':heap_max,
+                                                'heap_free':heap_free,
+                                                'non_heap_used':non_heap_usage,
+                                                'non_heap_committed':'Null',
+                                                'non_heap_max':non_heap_max,
+                                                'non_heap_free':non_heap_free,
                                                 'othermsg':othermsg,})
 
                             elif len(line.split('; ')) == 4:
@@ -134,7 +162,7 @@ class ITOM_OBM():
                                                                                                                   data.get('othermsg'))
                         sqldata.append(sql_insert)
                     except Exception as e:
-                        logSQLCreate.warning("Can't generate SQL INSERT INTO statement! Reason: " + str(e))
+                        logSQLCreate.warning("file:{}\nline:{}\nSource:{}\nException:{}".format(self.filepath,str(log_num), line, e))
 
                 self.SQLData = ({'db_name': self.db_name,
                                  'db_type': self.db_type,
